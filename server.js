49f3f5d3eeedfa9413c1e3eb70b18ca88532a53f
hassanmern <hassan.mern@gmail.com>
2023-09-12 20:28:00 +0000

    RESOLVED  - CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //my imports
 const db_initializer = require("./db/connection");
@@ -11,6 +12,12 @@ const port = process.env.BACKEND_PORT;
 
 //initialize express app
 var app = express();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.listen(port, () => {
   console.log(`Sysmart listening on port ${port}`);
 });
@@ -20,6 +27,19 @@ db_initializer();
 //initializing routes
 
 routes(app);
+
+// const whitelist = ["http://localhost:3000", "*"];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
+// app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
